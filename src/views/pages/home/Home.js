5cfd09ab1b7f8aa2753db5671570176059ca3fd8
PolarCoding <claytondwhite@gmail.com>
2020-11-04 08:14:35 -0800

    popular city, logo, favicon, footer

--- src/views/pages/home/Home.js
+++ src/views/pages/home/Home.js
@@ -1,29 +1,28 @@
-import React from 'react'
-import { Row, Col } from 'reactstrap'
+import React from "react"
+import { Row, Col } from "reactstrap"
 
-import Navbar from '../../../layouts/navbar/Navbar'
-import Footer from '../../../layouts/footer/Footer'
-import Content from './contents/Content'
-import MapModal from '../../ui-elements/modal/MapModal'
+import Navbar from "../../../layouts/navbar/Navbar"
+import Footer from "../../../layouts/footer/Footer"
+import Content from "./contents/Content"
+import MapModal from "../../ui-elements/modal/MapModal"
 
 export default function Home(props) {
-    
-    return (
-        <div id="main">
-            <Row className="match-height">
-                <Col lg="12" md="12" sm="12">
-                    <Navbar />
-                </Col>
-                <Col lg="12" md="12" sm="12">
-                    <Content />
-                </Col>
-                <Col lg="12" md="12" sm="12">
-                    <Footer />
-                </Col>
-                <Col lg="12" md="12" sm="12">
-                    <MapModal />
-                </Col>
-            </Row>
-        </div>
-    );
+  return (
+    <div id="main">
+      <Row className="match-height">
+        <Col lg="12" md="12" sm="12">
+          <Navbar />
+        </Col>
+        <Col lg="12" md="12" sm="12">
+          <Content />
+        </Col>
+        <Col lg="12" md="12" sm="12">
+          <Footer />
+        </Col>
+        <Col lg="12" md="12" sm="12">
+          <MapModal />
+        </Col>
+      </Row>
+    </div>
+  )
 }

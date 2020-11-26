0f697090c04c8c387396ee293fae6428e6493281
Bhavin sen <senbhavin@gmail.com>
2020-11-26 17:56:21 +0530

    Implemente validation for register domain

--- src/index.js
+++ src/index.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './redux';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import axios from "axios";
+
+import rootReducer from "./redux";
+import * as serviceWorker from "./serviceWorker";
+import ErrorPage from "./404"
+
+import "./index.css";
+
+import App from "./App";
 
 const store = createStore(rootReducer, composeWithDevTools());
-
-ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
-  document.getElementById('root')
-);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+const getConfig = () => {
+  return new Promise((resolve, reject) => {
+    var subdomain = window.location.host.split('.')[0];
+    axios
+      .get(`${process.env.REACT_APP_API_URL}v1.0/agency/${subdomain}`)
+      .then((response) => resolve(response)).catch(err => reject(err));
+  });
+};
+
+getConfig().then(() => {
+  ReactDOM.render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
+}).catch(err => ReactDOM.render(<ErrorPage />,
+  document.getElementById("root")
+))
+
 serviceWorker.unregister();
